07bb4d34f85445704279e7f7c3fe03094a75602f
Jerrin Jacob <jacob.jerrin@gmail.com>
2018-06-15 10:02:58 -0500

    List app loading names, desc and prices - but app is crashing

--- ListApp/app/src/main/java/jacob/jerrin/listapp/MainActivity.java
+++ ListApp/app/src/main/java/jacob/jerrin/listapp/MainActivity.java
@@ -1,13 +1,33 @@
 package jacob.jerrin.listapp;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String[] items;
+    String[] prices;
+    String[] descriptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        myListView = (ListView) findViewById(R.id.myListView);
+        // getting the array from the activity
+        items = res.getStringArray(R.array.items);
+        prices = res.getStringArray(R.array.prices);
+        descriptions = res.getStringArray(R.array.descriptions);
+
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, items, descriptions, prices);
+        myListView.setAdapter(itemAdapter);
+
+
     }
 }
